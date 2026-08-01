550452cc32861170d3a1525980814e3fac014026
srushti-1507 <srushtimenthe@gmail.com>
2026-08-01 13:47:19 +0000

    p11

---
+++ p11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a year ";
+    cin>>a;
+    if(a%4==0)
+    cout<<"year is leap year";
+    else
+    cout<<"year is not a leap year";
+}
