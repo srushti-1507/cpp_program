0a6e71b2ae3646d45eb1a7f52d0ca5031ba74e67
srushti-1507 <srushtimenthe@gmail.com>
2026-08-01 13:22:05 +0000

    p8

---
+++ p8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a and b number";
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater than b";
+    
+    }
+    else{
+        cout<<"b is greater than a";
+        
+    }
+}
