f11966595e2d20c8f8b3e54d2f9e0ab6b25604e8
srushti-1507 <srushtimenthe@gmail.com>
2026-08-07 05:51:52 +0000

    p2

---
+++ p2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter 1st number:"<<endl;
+    cin>>n1;
+    cout<<"Enter 2nd number:"<<endl;
+    cin>>n2;
+    cout<<"Sum of the numbers is: "<<n1+n2<<endl;
+}
